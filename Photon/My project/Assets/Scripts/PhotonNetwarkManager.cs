8760879f8467e314105bbbbfc82c9360ecd2d3f0
JBiswin <163123735+JBiswin@users.noreply.github.com>
2024-10-15 15:37:24 +0900

    2024.10.15

---
+++ Photon/My project/Assets/Scripts/PhotonNetwarkManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PhotonNetwarkManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField emailInputField;
+    [SerializeField] InputField passwordInputField;
+
+    public void Success()
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        PhotonNetwork.GameVersion = "1.0f";
+
+        PhotonNetwork.LoadLevel("Lobby Scene");
+
+
+    }
+
+
+}
